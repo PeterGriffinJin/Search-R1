54f5bb8603fe6d45fee79499f7e92ebfdded2263
HansiZeng <hanszenghappy@gmail.com>
2025-03-23 15:46:49 +0000

    halfway of add reranker

--- search_r1/llm_agent/generation.py
+++ search_r1/llm_agent/generation.py
@@ -21,6 +21,14 @@ class GenerationConfig:
     no_think_rl: bool=False
     search_url: str = None
     topk: int = 3
+    
+    do_rerank: bool = False
+    rerank_url: str = None
+    rerank_topk: int = 3
+    
+    def __post_init__(self):
+        if self.do_rerank:
+            assert self.topk >= self.rerank_topk, "topk {} should be larger than rerank_topk {}".format(self.topk, self.rerank_topk)
 
 class LLMGenerationManager:
     def __init__(
@@ -350,7 +358,12 @@ class LLMGenerationManager:
         
         return final_output
 
-    def execute_predictions(self, predictions: List[str], pad_token: str, active_mask=None, do_search=True) -> List[str]:
+    def execute_predictions(self, 
+                            predictions: List[str], 
+                            pad_token: str, 
+                            active_mask=None, 
+                            do_search=True,
+                            do_rerank=False) -> List[str]:
         """
         Execute predictions across multiple environments.
         NOTE: the function is the actual `step` function in the environment
@@ -371,6 +384,9 @@ class LLMGenerationManager:
         if do_search:
             search_results = self.batch_search(search_queries)
             assert len(search_results) == sum([1 for action in cur_actions if action == 'search'])
+            
+            if do_rerank:
+                
         else:
             search_results = [''] * sum([1 for action in cur_actions if action == 'search'])
 
@@ -434,6 +450,34 @@ If I want to give the final answer, I should put the answer between <answer> and
             contents.append(content)
             
         return actions, contents
+    
+    def batch_search_and_rerank(self, queries: List[str] = None) -> str: 
+        """
+        The extension of batch_search to include reranking 
+        Args:
+            queries: list of str 
+        Returns:
+            List of str where each str is the concatenated search+rerank results
+        """
+        results = self._batch_search(queries)["result"] # list of list of dict
+        
+    def _batch_rerank(self, queries: List[str] = None, docs: List[List[Dict]] = None):
+        """
+        Batchified rerank for queries and docs.
+        Args:
+            queries: queries to call the search engine
+            docs: list of list of dict
+        Returns:
+            rerank results which is concatenated into a string
+        """
+        payload = {
+            "queries": queries,
+            "docs": docs,
+            "topk": self.config.rerank_topk,
+            "return_scores": True
+        }
+        
+        return requests.post(self.config.rerank_url, json=payload).json()
 
     def batch_search(self, queries: List[str] = None) -> str:
         """
